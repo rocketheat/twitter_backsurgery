b2a1fc65aedd15806aad847e18ccd242cfbf48ee
rocketheat <rocketheat@gmail.com>
2018-01-16 17:06:09 -0500

    Adding Files and Folders

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# twitter_backsurgery
+# Twitter Back Surgery
+
+The key word for search is #backsurgery
+
+Usage:
+1. Install python anaconda from https://anaconda.org/anaconda/python
+2. create a folder and name it what you want
+3. download these files and unzip them
+4. copy the files to the folder you Created
+5. From the command line cd into the folder you created (example: cd /Documents/to/path)
+6. when you are in the folder, create an anaconda environment by conda env create -f environment.yml  
+7. activate the anaconda environement from the command line by source activate twitter or activate twitter (depend on operating system)
+8. run the file from terminal: python TwitterSchedule.py
+9. when enough data is collected the file data.txt you can stop the application by doing control C
---
+++ TwitterSchedule.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 08:03:24 2017
+
+@author: Ghaith Habboub
+
+@Topic: Twitter Query for Spine Surgery
+
+@Description: This is a twitter query for anyperson who mention spine surgery
+"""
+
+import twitter
+import json
+import schedule
+import time
+import pprint
+
+# These are the REST API parameters
+consumerKey= 'tFznFsSwD7rsfS1Xwnec50j2g'
+consumerSecret='oCrNm4FcmTiAr7ylIsWS0S4d6XHlO9hRmH4sLxKQ6JZDpOxQeu'
+OauthToken= '813509115423195136-ifRGdypYAIwpzAyxI0UayE7jHUWsteM'
+OauthSecret= 'Ja0Ju6rgbMYgVCDi3sRpG5eHHONIsBza3CKGx5krpVSP6'
+
+authinfo = twitter.oauth.OAuth(OauthToken, OauthSecret, consumerKey, consumerSecret)
+twitterAPI = twitter.Twitter(auth=authinfo)
+
+def twitterschedule():
+    topicsearch = twitterAPI.search.tweets(q="#backsurgery")
+    pprint.pprint(topicsearch)
+    with open('./data.txt', 'a') as outfile:
+        json.dump(topicsearch, outfile)
+    interval = """\n
+                  \n
+                  ***+++***
+                  \n
+                  \n"""
+
+schedule.every(180).minutes.do(twitterschedule)
+
+while True:
+    schedule.run_pending()
+    print('still working')
+    time.sleep(10)
